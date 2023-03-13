edc626121efb9d1ec2cace5fa2597ffeefd8506c
DevHyperCoder <devan@devhypercoder.com>
2023-03-13 10:56:38 +0530

    Add PreditOutput questions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use std::{fs::OpenOptions, io::Write, process::Command};
+use std::{fs::OpenOptions, io::Write, path::PathBuf, process::Command};
 
 const QPFILE: &str = "/tmp/exam.tex";
 const MSFILE: &str = "/tmp/marking.tex";
@@ -11,6 +11,74 @@ text given in the question.";
 
 const COURSE: &str = "EXAMSH";
 const TEST_NAME: &str = "Basic Test";
+
+struct PredictOutput<'a> {
+    question: &'a str,
+    code: Vec<(&'a str, &'a str)>,
+    run_cmd: fn(PathBuf) -> Result<String, String>,
+}
+
+impl<'a> PredictOutput<'a> {
+    fn render(&self) -> String {
+        let mut temp_dir = std::env::temp_dir();
+        temp_dir.push(format!("examsh-{}", Utc::now()));
+        std::fs::create_dir(&temp_dir).expect("Unable to create temp directory");
+
+        let code = &self
+            .code
+            .iter()
+            .map(|(fname, code)| {
+                let mut asdf = temp_dir.clone();
+                asdf.push(fname);
+                let mut f = OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .open(asdf)
+                    .expect("Unable to open file");
+                write!(f, "{}", code).expect("Unable to write to file");
+                println!("done");
+                f.flush().expect("Unable to flush");
+
+                format!(
+                    "
+\\textbf{{{}}}
+\\begin{{verbatim}}
+{}
+\\end{{verbatim}}
+",
+                    fname, code
+                )
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
+
+        let output = (self.run_cmd)(temp_dir);
+        if output.is_err() {
+            eprintln!("[CODE RUNNER] Error occured.");
+            "ERROR".to_string()
+        } else {
+            format!(
+                "
+\\question
+{}
+
+Code:
+
+{}
+
+\\begin{{solution}}
+\\begin{{verbatim}}
+{}
+\\end{{verbatim}}
+\\end{{solution}}
+",
+                self.question,
+                code,
+                output.unwrap()
+            )
+        }
+    }
+}
 
 struct MultipleChoiceQuestions<'a> {
     question: &'a str,
@@ -87,7 +155,37 @@ Enter the temperature in Kelvin: 300
 Enter the scale you want to convert TO (F, C): C
 300 K is 26.85 C"
     }.render();
-    format!("{}\n{}", q1, q2)
+    let q3 = PredictOutput {
+        question: "Find out output",
+        code: vec![(
+            "main.rs",
+            "
+fn main() {
+    println!(\"Hello World!\");
+}
+",
+        )],
+        run_cmd: |a| {
+            let mut path = a.clone();
+            path.push("main.rs");
+
+            Command::new("rustc")
+                .arg(path.to_string_lossy().to_string())
+                .arg(format!("--out-dir={}", a.to_string_lossy()))
+                .status()
+                .expect("Unable to compile rust code.");
+
+            path.pop();
+            path.push("main");
+
+            let o = Command::new(path).output().expect("Unable to run command");
+
+            let s = String::from_utf8(o.stdout).expect("Unexpected output from code.");
+            Ok(s)
+        },
+    }
+    .render();
+    format!("{}\n{}\n{}", q1, q2, q3)
 }
 
 fn render_latex(latexname: &str, out_dir: &str, content: &str) {
